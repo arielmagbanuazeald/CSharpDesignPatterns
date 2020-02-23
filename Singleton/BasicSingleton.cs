cc32a4729ff5e16c10dbb92ac8cde2dbfafd9202
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-02-23 18:56:29 +0800

    Added non thread safe singleton.

---
+++ Singleton/BasicSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns.Singleton
+{
+    /// <summary>
+    /// Non Thread Safe Singleton
+    /// Reference: https://www.c-sharpcorner.com/UploadFile/8911c4/singleton-design-pattern-in-C-Sharp/
+    /// </summary>
+    public sealed class BasicSingleton
+    {
+        private static BasicSingleton _instance = null;
+
+        private BasicSingleton()
+        {
+
+        }
+
+        public static BasicSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new BasicSingleton();
+                }
+
+                return _instance;
+            }
+        }
+
+        public string Name { get; set; }
+    }
+}
